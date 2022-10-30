9a2289209b02b6fa768f8b06f9a2bda1cff1dc1f
bikoo <bikoo3002@naver.com>
2022-10-30 19:20:31 +0900

    Initial commit

---
+++ src/main/java/com/example/tddreststudy/TddRestStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddreststudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddRestStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddRestStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tddreststudy/TddRestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddreststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddRestStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
